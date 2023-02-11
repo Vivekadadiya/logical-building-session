cec6a61d4b9e7eab981236bc416516555251406b
Bhumitraj <bhumitrajmahida@gmail.com>
2023-02-11 18:44:58 +0530

    Fix: minor change

--- test11-02-23.js
+++ test11-02-23.js
@@ -1,9 +1,10 @@
 let pattern = "";
 for (let i = 1; i <= 5; i++) {
-   for (let num = 1; num <= i; num++) {
-      pattern += 5-i+1;
-   }
-
-   pattern += "\n";
+    for (let num = 1; num <= i; num++) {
+        pattern += (5 - i + 1);
+    }
+    if (i < 5) {
+        pattern += "\n";
+    }
 }
 console.log(pattern);
