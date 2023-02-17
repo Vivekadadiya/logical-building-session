9d5ea05ae973c3e78f821cf06c23e4ad76e7aa66
Vivek Radadiya <98732891+Vivekadadiya@users.noreply.github.com>
2023-02-17 11:37:16 +0530

    practical 2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  useEffect(() => {
+    let patterArray = [1, 2, 3, 4, 5];
+
+    patterArray.map((item) => {
+      let result = "";
+      for (let num = 1; num <= item; num++) {
+        result += 5 - item + 1;
+      }
+      console.log(result);
+    });
+  }, []);
+
+  return <div className="App"></div>;
+}
+
+export default App;
